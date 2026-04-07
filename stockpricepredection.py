dde8046f1a3e06f2ea047ff14ef9700ec0830d55
Vijayalaxmi Barge <162585547+vijayalaxmi-barge@users.noreply.github.com>
2026-04-07 20:50:10 +0530

    Add copyright and license information
    Added copyright notice and GitHub link at the top of the file.

--- stockpricepredection.py
+++ stockpricepredection.py
@@ -1,4 +1,6 @@
-import pandas as pd 
+# // Copyright (c) 2026 Vijayalaxmi Barge
+# // GitHub: https://github.com/vijayalaxmi-barge
+# // Licensed under the MIT Licenseimport pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt 
 import panda_datareader as data
